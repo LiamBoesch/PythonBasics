3d2a24ac218eaae86bd2a751691df4ac2db89271
LiamBoesch <liam_boesch@sluz.ch>
2025-09-16 14:10:12 +0200

    aufgabe gelöst

--- task_1_1.py
+++ task_1_1.py
@@ -1,3 +1,3 @@
 # block 1 aufgabe 1
 name = input("Wie ist dein Name? ")
-print("Hallo " + name)
+print(f"Hallo {name}!")
--- task_1_2.py
+++ task_1_2.py
@@ -1,5 +1,5 @@
 #block 1 augabe 2
 zahl1 = input("was ist die erste zahl?")
 zahl2 = input("was ist die zweite zahl?")
-summe = (int(zahl1) + int(zahl2))
-print(("die summe ist ") + str(summe))
+summe = (float(zahl1) + float(zahl2))
+print((f"die summe ist {summe}"))
