9b3a6d2f8df089236e4bf844d39c10b305f6e68e
LiamBoesch <liam_boesch@sluz.ch>
2025-09-23 13:26:07 +0200

    updatet task 5.1

---
+++ Task_2_2.py
@@ -0,0 +1,7 @@
+# = beduetet gleich
+# == beduetet ist gleich
+# != beduetet ungleich  
+# > beduetet groesser als
+# < beduetet kleiner als
+# >= beduetet groesser gleich
+# <= beduetet kleiner gleich
---
+++ task_5_1.py
@@ -0,0 +1,5 @@
+x = 5
+ergebnis1 = int(x) + 1 
+print(ergebnis1)
+ergebnis2 = int(ergebnis1) - 2
+print(ergebnis2)
